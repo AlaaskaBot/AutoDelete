dbbcc9782d71af8897364e54f8e11bb0a8da565f
AlaaskaBot <123285025+AlaaskaBot@users.noreply.github.com>
2025-06-27 07:43:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,42 @@
-#=========================================================================
-# [AutoDelete - Telegram bot to delete messages after specific time]      
-# Copyright (C) 2022 Arunkumar Shibu                       
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#=========================================================================
-
-from time import time 
-from utils.info import *
-from utils.database import *
+from time import time
+from os import getenv
 from subprocess import Popen
 from pyrogram import Client, filters
+from utils.info import *
+from utils.database import *
 
-User = Client("auto-delete-user",
-              session_string=SESSION)
+# Read API credentials from environment
+api_id = int(getenv("API_ID"))
+api_hash = getenv("API_HASH")
+bot_token = getenv("BOT_TOKEN")
+port = int(getenv("PORT", 8080))  # default fallback if PORT is not set
+
+User = Client(
+    "auto-delete-user",
+    api_id=api_id,
+    api_hash=api_hash,
+    bot_token=bot_token
+)
 
 @User.on_message(filters.chat(CHATS))
 async def delete(user, message):
     try:
-       if bool(WHITE_LIST):
-          if message.from_user.id in WHITE_LIST:
-             return 
-       if bool(BLACK_LIST):
-          if message.from_user.id not in BLACK_LIST:
-             return
-       _time = int(time()) + TIME 
-       save_message(message, _time)
+        if bool(WHITE_LIST):
+            if message.from_user.id in WHITE_LIST:
+                return 
+        if bool(BLACK_LIST):
+            if message.from_user.id not in BLACK_LIST:
+                return
+        _time = int(time()) + TIME 
+        save_message(message, _time)
     except Exception as e:
-       print(str(e))
+        print(str(e))
 
 @User.on_message(filters.regex("!start") & filters.private)
 async def start(user, message):
     await message.reply("Hi, I'm alive!")
 
-#==========================================================
-
-Popen(f"gunicorn utils.server:app --bind 0.0.0.0:{PORT}", shell=True)
+# Start the server and the delete script
+Popen(f"gunicorn utils.server:app --bind 0.0.0.0:{port}", shell=True)
 Popen("python3 -m utils.delete", shell=True)
 User.run()
